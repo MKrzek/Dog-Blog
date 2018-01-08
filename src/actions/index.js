f89006c1b80ffb446f35561ecaa8b7caa69a87e4
Malgorzata <mkrzek@googlemail.com>
2018-01-08 15:06:10 +0000

    second-commit

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import * as firebase from 'firebase';
+const config = {
+    apiKey: "AIzaSyAW2Ju7jK7YGKn0qZtmCp7u7dTB2lvgJCs",
+    authDomain: "dog-blog-50b2a.firebaseapp.com",
+    databaseURL: "https://dog-blog-50b2a.firebaseio.com",
+    projectId: "dog-blog-50b2a",
+    storageBucket: "",
+    messagingSenderId: "925286955795"
+}
+const firebaseApp=firebase.initializeApp(config);
