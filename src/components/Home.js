a2e840f02b467f788bab32b7e4171c4acc4afc7f
Malgorzata <mkrzek@googlemail.com>
2018-01-11 15:26:20 +0000

    added key-adding from firebase to object as an id

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
-
 import {connect} from 'react-redux';
 import * as Actions from '../actions/index.js';
 import _ from 'lodash';
 
 import Navigation from './Navigation.js';
 import ArticleData from './ArticleData.js';
+
 class Home extends React.Component{
 
 componentDidMount(){
@@ -15,13 +14,16 @@ componentDidMount(){
 }
 
  showArticle=()=>{
-     console.log ('props.article', this.props.articles)
-            return  _.map(this.props.articles, article => {
-                console.log ('lodash', article)
-                return <ArticleData article={article}/>
-           })   
-    }  
-
+      for (const key of Object.keys(this.props.articles)){
+          console.log (key, this.props.articles[key])
+          this.props.articles[key].id=(key)
+          console.log (this.props.articles)
+      }
+        return _.map(this.props.articles, article=>{
+            return <ArticleData article={article} key={article.id}/>
+        })
+    }
+      
     render(){
       
         return <div>
@@ -39,7 +41,7 @@ componentDidMount(){
 }
 }
 function mapStateToProps(state){
-    console.log ('displayArticle', state.displayArticle)
+   
     return {articles : state.displayArticle}
 }
 export default connect (mapStateToProps, Actions)(Home);
--- src/reducers/reducer_displayArt.js
+++ src/reducers/reducer_displayArt.js
@@ -1,6 +1,7 @@
 import{DISPLAY_ARTICLES} from'../constants.js';
 
 export default function (state=[], action){
+    
     switch(action.type){
         case DISPLAY_ARTICLES:
             return action.payload;
