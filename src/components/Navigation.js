3cdeeb14dba7ba164a21d52873a15159bd0147ec
Malgorzata <mkrzek@googlemail.com>
2018-01-31 17:19:10 +0000

    refactored code in navgation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,22 +3,29 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import * as Actions from "../actions/index.js";
 
-class Navigation extends React.Component {
+class Navigation1 extends React.Component {
   handleSignOut() {
     this.props.signOutUser();
   }
-
   render() {
     return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <Link to="/home" className="navbar-brand text-danger">
-              Dogether
-            </Link>
-          </div>
-
-          <ul className="nav navbar-nav navbar-right" id="navig">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand text-danger" to="/home">
+          Dogether
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav navbar-right nav">
             <li className="nav-item" key={1}>
               <Link className="nav-link text-danger" to="/gallery">
                 Gallery
@@ -39,23 +46,25 @@ class Navigation extends React.Component {
                 Adoption
               </Link>
             </li>
-            <li className='nav-item' key={5}>
-             <Link className='nav-link text-danger' to='/myAccount'>
-               My Account
-               </Link>
-               </li>
+            <li className="nav-item" key={5}>
+              <Link className="nav-link text-danger" to="/myAccount">
+                My Account
+              </Link>
+            </li>
             <li className="nav-item" key={6}>
               <a
                 id="signout"
                 className="nav-link text-danger"
                 onClick={() => this.handleSignOut()}
-              >Sign Out</a>
+              >
+                Sign Out
+              </a>
             </li>
-
           </ul>
         </div>
       </nav>
     );
   }
 }
-export default connect(null, Actions)(Navigation);
+
+export default connect(null, Actions)(Navigation1);
